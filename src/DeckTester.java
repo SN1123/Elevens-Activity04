2061c88839b19ba8f1b3e1003904c7d3f31c782a
Kyle Waltz <kylewaltz@yahoo.com>
2015-03-12 07:56:04 -0700

    Update DeckTester.java

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -1,56 +1,110 @@
+import java.util.List;
+import java.util.ArrayList;
 /**
- * This is a class that tests the Deck class.
+ * The Deck class represents a shuffled deck of cards.
+ * It provides several operations including
+ * initialize, shuffle, deal, and check if empty.
  */
-public class DeckTester {
-
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		String[] ranks = {"jack", "queen", "king"};
-		String[] suits = {"blue", "red"};
-		int[] pointValues = {11, 12, 13};
-		Deck d = new Deck(ranks, suits, pointValues);
-
-		System.out.println("**** Original Deck Methods ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
-
-		System.out.println("**** Deal a Card ****");
-		System.out.println("  deal: " + d.deal());
-		System.out.println();
-		System.out.println();
-
-		System.out.println("**** Deck Methods After 1 Card Dealt ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
-
-		System.out.println("**** Deal Remaining 5 Cards ****");
-		for (int i = 0; i < 5; i++) {
-			System.out.println("  deal: " + d.deal());
-		}
-		System.out.println();
-		System.out.println();
-
-		System.out.println("**** Deck Methods After All Cards Dealt ****");
-		System.out.println("  toString:\n" + d.toString());
-		System.out.println("  isEmpty: " + d.isEmpty());
-		System.out.println("  size: " + d.size());
-		System.out.println();
-		System.out.println();
-
-		System.out.println("**** Deal a Card From Empty Deck ****");
-		System.out.println("  deal: " + d.deal());
-		System.out.println();
-		System.out.println();
-
-		/* *** TO BE COMPLETED IN ACTIVITY 4 *** */
-	}
+public class Deck {
+  /**
+   * cards contains all the cards in the deck.
+   */
+  private List<Card> cards;
+  /**
+   * size is the number of not-yet-dealt cards.
+   * Cards are dealt from the top (highest index) down.
+   * The next card to be dealt is at size - 1.
+   */
+  private int size;
+  /**
+   * Creates a new <code>Deck</code> instance.<BR>
+   * It pairs each element of ranks with each element of suits,
+   * and produces one of the corresponding card.
+   * @param ranks is an array containing all of the card ranks.
+   * @param suits is an array containing all of the card suits.
+   * @param values is an array containing all of the card point values.
+   */
+  public Deck(String[] ranks, String[] suits, int[] values) {
+    cards = new ArrayList<Card>();
+    for (int j = 0; j < ranks.length; j++) {
+      for (String suitString : suits) {
+        cards.add(new Card(ranks[j], suitString, values[j]));
+      }
+    }
+    size = cards.size();
+    shuffle();
+  }
+  /**
+   * Determines if this deck is empty (no undealt cards).
+   * @return true if this deck is empty, false otherwise.
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+  /**
+   * Accesses the number of undealt cards in this deck.
+   * @return the number of undealt cards in this deck.
+   */
+  public int size() {
+    return size;
+  }
+  /**
+   * Randomly permute the given collection of cards
+   * and reset the size to represent the entire deck.
+   */
+  public void shuffle() {
+    /* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+    for(int i=cards.size()-1;i>0;i--)
+    {
+      int k = (int)Math.random()*i;
+      Card temp = cards.get(i);
+      cards.set(i, cards.get(k));
+      cards.set(k, temp);
+    }
+    
+  }
+  /**
+   * Deals a card from this deck.
+   * @return the card just dealt, or null if all the cards have been
+   * previously dealt.
+   */
+  public Card deal() {
+    if (isEmpty()) {
+      return null;
+    }
+    size--;
+    Card c = cards.get(size);
+    return c;
+  }
+  /**
+   * Generates and returns a string representation of this deck.
+   * @return a string representation of this deck.
+   */
+  @Override
+  public String toString() {
+    String rtn = "size = " + size + "\nUndealt cards: \n";
+    for (int k = size - 1; k >= 0; k--) {
+      rtn = rtn + cards.get(k);
+      if (k != 0) {
+        rtn = rtn + ", ";
+      }
+      if ((size - k) % 2 == 0) {
+// Insert carriage returns so entire deck is visible on console.
+        rtn = rtn + "\n";
+      }
+    }
+    rtn = rtn + "\nDealt cards: \n";
+    for (int k = cards.size() - 1; k >= size; k--) {
+      rtn = rtn + cards.get(k);
+      if (k != size) {
+        rtn = rtn + ", ";
+      }
+      if ((k - cards.size()) % 2 == 0) {
+// Insert carriage returns so entire deck is visible on console.
+        rtn = rtn + "\n";
+      }
+    }
+    rtn = rtn + "\n";
+    return rtn;
+  }
 }
